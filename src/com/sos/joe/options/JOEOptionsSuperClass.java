6e6be74230316e2126724cab39f5f8ffd9e4f199
Santiago Aucejo Petzoldt <santiago.aucejo.petzoldt.git@sos-berlin.com>
2014-05-08 09:05:40 +0000

    throws Exception removed from overriding Methods [SP]

--- src/com/sos/joe/options/JOEOptionsSuperClass.java
+++ src/com/sos/joe/options/JOEOptionsSuperClass.java
@@ -197,7 +197,7 @@ public class JOEOptionsSuperClass extends JSOptionsClass {
 	 * @throws Exception
 	 */
 	@Override
-	public void setAllOptions(final HashMap<String, String> pobjJSSettings) throws Exception {
+	public void setAllOptions(final HashMap<String, String> pobjJSSettings) {
 		@SuppressWarnings("unused")
 		final String conMethodName = conClassName + "::setAllOptions";
 		flgSetAllOptions = true;
@@ -242,7 +242,7 @@ public class JOEOptionsSuperClass extends JSOptionsClass {
 	 * @throws Exception
 	 */
 	@Override
-	public void CommandLineArgs(final String[] pstrArgs) throws Exception {
+	public void CommandLineArgs(final String[] pstrArgs) {
 		super.CommandLineArgs(pstrArgs);
 		this.setAllOptions(super.objSettings);
 	}
