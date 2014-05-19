e925b54dede57de9e3c0ced3f3e31abdbf517b25
Santiago Aucejo Petzoldt <santiago.aucejo.petzoldt.git@sos-berlin.com>
2014-05-19 10:50:03 +0000

    Maven migration

--- src/com/sos/jobdoc/IDocumentationUpdate.java
+++
@@ -1,9 +0,0 @@
-package com.sos.jobdoc;
-
-import com.sos.joe.interfaces.IDataChanged;
-
-public interface IDocumentationUpdate extends IDataChanged {
-	
-	public void updateReleases();
-
-}
--- src/com/sos/joe/interfaces/IUnsaved.java
+++
@@ -1,8 +0,0 @@
-package com.sos.joe.interfaces;
-
-public interface IUnsaved {
-    public boolean isUnsaved();
-
-
-    public void apply();
-}
