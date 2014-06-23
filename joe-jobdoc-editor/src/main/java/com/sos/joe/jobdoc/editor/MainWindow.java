6532c9ee254b637ab959e424e6112859fec11879
sos <sos@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2014-06-23 07:48:49 +0000

    Klasse com.sos.JSHelper.Basics.JSVersionInfo durch com.sos.JSHelper.Basics.VersionInfo ersetzt

--- joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/MainWindow.java
+++ joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/MainWindow.java
@@ -1,40 +1,5 @@
 package com.sos.joe.jobdoc.editor;
-import static sos.util.SOSClassUtil.getMethodName;
-
-import java.io.File;
-import java.util.ArrayList;
-
-import org.apache.log4j.Logger;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabFolder2Adapter;
-import org.eclipse.swt.custom.CTabFolderEvent;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.events.ShellEvent;
-import org.eclipse.swt.events.ShellListener;
-import org.eclipse.swt.events.TraverseEvent;
-import org.eclipse.swt.events.TraverseListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.ToolBar;
-import org.eclipse.swt.widgets.ToolItem;
-
-import com.sos.JSHelper.Basics.JSVersionInfo;
+import com.sos.JSHelper.Basics.VersionInfo;
 import com.sos.dialog.classes.WindowsSaver;
 import com.sos.i18n.annotation.I18NMessage;
 import com.sos.i18n.annotation.I18NMessages;
@@ -46,6 +11,24 @@ import com.sos.joe.globals.misc.ResourceManager;
 import com.sos.joe.globals.options.Options;
 import com.sos.joe.jobdoc.editor.forms.DocumentationForm;
 import com.sos.joe.xml.Utils;
+import org.apache.log4j.Logger;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabFolder2Adapter;
+import org.eclipse.swt.custom.CTabFolderEvent;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static sos.util.SOSClassUtil.getMethodName;
 
 @I18NResourceBundle(baseName = "JOEMessages", defaultLocale = "en") public class MainWindow {
 	private static final String	conStringEDITOR			= "editor";
@@ -378,7 +361,7 @@ import com.sos.joe.xml.Utils;
 		groupmain.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, false));
 		createSShell(shell, groupmain);
 		Options.loadWindow(sShell, conStringEDITOR);
-		String strT = Messages.getLabel(JOE_I_0010) + JSVersionInfo.conVersionNumber;
+		String strT = Messages.getLabel(JOE_I_0010) + " " + VersionInfo.JAR_VERSION;
 		//		container.setTitleText(strT);
 		sShell.setText(strT);
 		logger.debug(strT);
