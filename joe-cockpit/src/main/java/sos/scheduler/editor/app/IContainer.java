11c35edf8f4630189a3ac6acd1fc83dfc16af3c9
Uwe Risse <uwe.risse-git@sos-berlin.com>
2015-05-13 15:59:13 +0200

    JOE-140

--- joe-cockpit/src/main/java/sos/scheduler/editor/app/IContainer.java
+++ joe-cockpit/src/main/java/sos/scheduler/editor/app/IContainer.java
@@ -11,46 +11,24 @@ import com.sos.joe.jobdoc.editor.forms.DocumentationForm;
 
 public interface IContainer {
 	public SchedulerForm newScheduler();
-
 	public SchedulerForm newScheduler(int type);
-
 	public DocumentationForm newDocumentation();
-	// public SchedulerForm reOpenScheduler(String filename);
 	public DocumentationForm openDocumentation();
-	public DocumentationForm openDocumentation(String filename);
-
+    public DocumentationForm openDocumentation(String filename);
 	public SchedulerForm openScheduler();
-
 	public org.eclipse.swt.widgets.Composite openQuick();
-
 	public org.eclipse.swt.widgets.Composite openQuick(String filename);
-
 	public SchedulerForm openScheduler(String filename);
-
-
-
 	public String openDocumentationName();
-
 	public IEditor getCurrentEditor();
-
 	public void setStatusInTitle();
-
 	public void setNewFilename(String oldFilename);
-
 	public boolean closeAll();
-
 	public void updateLanguages();
-
 	public JobChainConfigurationForm newDetails();
-
 	public JobChainConfigurationForm openDetails();
-
-	//public SchedulerForm openDirectory();
 	public SchedulerForm openDirectory(String filename);
-
 	public CTabItem getCurrentTab();
-
 	public ActionsForm newActions();
-
 	public void setTitleText(String strT);
 }
