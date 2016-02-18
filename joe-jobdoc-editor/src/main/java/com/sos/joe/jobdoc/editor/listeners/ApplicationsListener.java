3f9d2730bdf7c35cf5b1a1e0fe3e56df0ef54514
Uwe Risse <uwe.risse-git@sos-berlin.com>
2016-02-18 12:05:08 +0100

    JOE-201

--- joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/listeners/ApplicationsListener.java
+++ joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/listeners/ApplicationsListener.java
@@ -1,90 +1,93 @@
 package com.sos.joe.jobdoc.editor.listeners;
-import java.util.Iterator;
 
+import java.util.Iterator;
+import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.jdom.Element;
-
 import com.sos.joe.xml.ElementWrapper;
 import com.sos.joe.xml.jobdoc.DocumentationDom;
 
 public class ApplicationsListener extends JobDocBaseListener<DocumentationDom> {
-	private Element				_application;
-	private boolean				_newApp;
 
-	public ApplicationsListener(DocumentationDom dom, Element parent) {
-		_dom = dom;
-		_parent = parent;
-	}
+    private final static Logger LOGGER = Logger.getLogger(ApplicationsListener.class);
+    private Element _application;
+    private boolean _newApp;
 
-	public void fillApps(Table table) {
-		table.removeAll();
-		int index = 0;
-		for (Iterator it = _parent.getChildren("application", _dom.getNamespace()).iterator(); it.hasNext();) {
-			ElementWrapper objWr = new ElementWrapper((Element) it.next());
-			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText(0, objWr.getAttributeValue("name"));
-			item.setText(1, objWr.getAttributeValue("id"));
-			item.setText(2, objWr.getAttributeValue("reference"));
-			if (objWr.getElement().equals(_application))
-				table.select(index);
-			index++;
-		}
-	}
+    public ApplicationsListener(DocumentationDom dom, Element parent) {
+        _dom = dom;
+        _parent = parent;
+    }
 
-	public void setNewApp() {
-		_application = new Element("application", _dom.getNamespace());
-		_newApp = true;
-	}
+    public void fillApps(Table table) {
+        table.removeAll();
+        int index = 0;
+        for (Iterator it = _parent.getChildren("application", _dom.getNamespace()).iterator(); it.hasNext();) {
+            ElementWrapper objWr = new ElementWrapper((Element) it.next());
+            TableItem item = new TableItem(table, SWT.NONE);
+            item.setText(0, objWr.getAttributeValue("name"));
+            item.setText(1, objWr.getAttributeValue("id"));
+            item.setText(2, objWr.getAttributeValue("reference"));
+            if (objWr.getElement().equals(_application)) {
+                table.select(index);
+            }
+            index++;
+        }
+    }
 
-	public boolean selectApp(int index) {
-		try {
-			_application = (Element) _parent.getChildren("application", _dom.getNamespace()).get(index);
-			_newApp = false;
-			return true;
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public void setNewApp() {
+        _application = new Element("application", _dom.getNamespace());
+        _newApp = true;
+    }
 
-	public String getName() {
-		return getAttributeValue("name", _application);
-	}
+    public boolean selectApp(int index) {
+        try {
+            _application = (Element) _parent.getChildren("application", _dom.getNamespace()).get(index);
+            _newApp = false;
+            return true;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public String getID() {
-		return getAttributeValue("id", _application);
-	}
+    public String getName() {
+        return getAttributeValue("name", _application);
+    }
 
-	public String getReference() {
-		return getAttributeValue("reference", _application);
-	}
+    public String getID() {
+        return getAttributeValue("id", _application);
+    }
 
-	public String[] getReferences(String ownID) {
-		return DocumentationListener.getIDs(_dom, ownID);
-	}
+    public String getReference() {
+        return getAttributeValue("reference", _application);
+    }
 
-	public void applyApp(String name, String id, String reference) {
-		setAttribute("name", name, _application);
-		setAttribute("id", id, _application);
-		setAttribute("reference", DocumentationListener.getID(reference), _application);
-		if (_newApp)
-			_parent.addContent(_application);
-		_newApp = false;
-		_dom.setChanged(true);
-	}
+    public String[] getReferences(String ownID) {
+        return DocumentationListener.getIDs(_dom, ownID);
+    }
 
-	public boolean removeApp(int index) {
-		if (selectApp(index)) {
-			_application.detach();
-			_application = null;
-			_newApp = false;
-			_dom.setChanged(true);
-			return true;
-		}
-		else
-			return false;
-	}
+    public void applyApp(String name, String id, String reference) {
+        setAttribute("name", name, _application);
+        setAttribute("id", id, _application);
+        setAttribute("reference", DocumentationListener.getID(reference), _application);
+        if (_newApp) {
+            _parent.addContent(_application);
+        }
+        _newApp = false;
+        _dom.setChanged(true);
+    }
+
+    public boolean removeApp(int index) {
+        if (selectApp(index)) {
+            _application.detach();
+            _application = null;
+            _newApp = false;
+            _dom.setChanged(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/listeners/SettingListener.java
+++ joe-jobdoc-editor/src/main/java/com/sos/joe/jobdoc/editor/listeners/SettingListener.java
@@ -1,120 +1,122 @@
 package com.sos.joe.jobdoc.editor.listeners;
-import java.util.Iterator;
 
+import java.util.Iterator;
+import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.jdom.Element;
-
 import com.sos.joe.xml.jobdoc.DocumentationDom;
 
 public class SettingListener extends JobDocBaseListener<DocumentationDom> {
-	private Element					_setting;
-	private boolean					_newSetting;
-	private static final String[]	_types	= { "", "integer", "double", "float", "string", "boolean", "clob", "blob" };
 
-	public SettingListener(DocumentationDom dom, Element parent) {
-		_dom = dom;
-		_parent = parent;
-	}
+    private final static Logger LOGGER = Logger.getLogger(SettingListener.class);
+    private Element _setting;
+    private boolean _newSetting;
+    private static final String[] _types = { "", "integer", "double", "float", "string", "boolean", "clob", "blob" };
 
-	public void fillSettings(Table table) {
-		table.removeAll();
-		int index = 0;
-		for (Iterator it = _parent.getChildren("setting", _dom.getNamespace()).iterator(); it.hasNext();) {
-			Element setting = (Element) it.next();
-			TableItem item = new TableItem(table, SWT.NONE);
-			item.setText(0, getAttributeValue("name", setting));
-			item.setText(1, getAttributeValue("default_value", setting));
-			item.setText(2, getAttributeValue("type", setting));
-			item.setText(3, getBooleanValue("required", setting) ? "yes" : "no");
-			item.setText(4, getAttributeValue("reference", setting));
-			item.setText(5, getAttributeValue("id", setting));
-			if (setting.equals(_setting))
-				table.select(index);
-			index++;
-		}
-	}
+    public SettingListener(DocumentationDom dom, Element parent) {
+        _dom = dom;
+        _parent = parent;
+    }
 
-	public void setNewSetting() {
-		_setting = new Element("setting", _dom.getNamespace());
-		_newSetting = true;
-	}
+    public void fillSettings(Table table) {
+        table.removeAll();
+        int index = 0;
+        for (Iterator it = _parent.getChildren("setting", _dom.getNamespace()).iterator(); it.hasNext();) {
+            Element setting = (Element) it.next();
+            TableItem item = new TableItem(table, SWT.NONE);
+            item.setText(0, getAttributeValue("name", setting));
+            item.setText(1, getAttributeValue("default_value", setting));
+            item.setText(2, getAttributeValue("type", setting));
+            item.setText(3, getBooleanValue("required", setting) ? "yes" : "no");
+            item.setText(4, getAttributeValue("reference", setting));
+            item.setText(5, getAttributeValue("id", setting));
+            if (setting.equals(_setting))
+                table.select(index);
+            index++;
+        }
+    }
 
-	public boolean selectSetting(int index) {
-		try {
-			_setting = (Element) _parent.getChildren("setting", _dom.getNamespace()).get(index);
-			_newSetting = false;
-			return true;
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    public void setNewSetting() {
+        _setting = new Element("setting", _dom.getNamespace());
+        _newSetting = true;
+    }
 
-	public String getName() {
-		return getAttributeValue("name", _setting);
-	}
+    public boolean selectSetting(int index) {
+        try {
+            _setting = (Element) _parent.getChildren("setting", _dom.getNamespace()).get(index);
+            _newSetting = false;
+            return true;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return false;
+        }
+    }
 
-	public String getDefaultValue() {
-		return getAttributeValue("default_value", _setting);
-	}
+    public String getName() {
+        return getAttributeValue("name", _setting);
+    }
 
-	public String getID() {
-		return getAttributeValue("id", _setting);
-	}
+    public String getDefaultValue() {
+        return getAttributeValue("default_value", _setting);
+    }
 
-	public boolean isRequired() {
-		return getBooleanValue("required", _setting);
-	}
+    public String getID() {
+        return getAttributeValue("id", _setting);
+    }
 
-	public String getReference() {
-		return getAttributeValue("reference", _setting);
-	}
+    public boolean isRequired() {
+        return getBooleanValue("required", _setting);
+    }
 
-	public String getType() {
-		return getAttributeValue("type", _setting);
-	}
+    public String getReference() {
+        return getAttributeValue("reference", _setting);
+    }
 
-	public String[] getTypes() {
-		return _types;
-	}
+    public String getType() {
+        return getAttributeValue("type", _setting);
+    }
 
-	public Element getSettingElement() {
-		return _setting;
-	}
+    public String[] getTypes() {
+        return _types;
+    }
 
-	public boolean isNewSetting() {
-		return _newSetting;
-	}
+    public Element getSettingElement() {
+        return _setting;
+    }
 
-	public String[] getReferences(String ownID) {
-		return DocumentationListener.getIDs(_dom, ownID);
-	}
+    public boolean isNewSetting() {
+        return _newSetting;
+    }
 
-	public void applySetting(String name, String defaultValue, String id, boolean required, String reference, String type) {
-		setAttribute("name", name, _setting);
-		setAttribute("default_value", defaultValue, _setting);
-		setAttribute("id", id, _setting);
-		setBoolean("required", required, _setting);
-		setAttribute("reference", DocumentationListener.getID(reference), _setting);
-		setAttribute("type", type, _setting);
-		if (_newSetting)
-			_parent.addContent(_setting);
-		_newSetting = false;
-		_dom.setChanged(true);
-	}
+    public String[] getReferences(String ownID) {
+        return DocumentationListener.getIDs(_dom, ownID);
+    }
 
-	public boolean removeSetting(int index) {
-		if (selectSetting(index)) {
-			_setting.detach();
-			_setting = null;
-			_newSetting = false;
-			_dom.setChanged(true);
-			return true;
-		}
-		else
-			return false;
-	}
+    public void applySetting(String name, String defaultValue, String id, boolean required, String reference, String type) {
+        setAttribute("name", name, _setting);
+        setAttribute("default_value", defaultValue, _setting);
+        setAttribute("id", id, _setting);
+        setBoolean("required", required, _setting);
+        setAttribute("reference", DocumentationListener.getID(reference), _setting);
+        setAttribute("type", type, _setting);
+        if (_newSetting) {
+            _parent.addContent(_setting);
+        }
+        _newSetting = false;
+        _dom.setChanged(true);
+    }
+
+    public boolean removeSetting(int index) {
+        if (selectSetting(index)) {
+            _setting.detach();
+            _setting = null;
+            _newSetting = false;
+            _dom.setChanged(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
